2708707b655ef1ece925668629677ccc8ad1b5be
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-05-22 22:49:16 +0700

    init project and config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Fix Flow Service</h1>
+      <p>ระบบงานซ่อมของคุณดี</p>
+    </div>
+  );
+}
+export default App;
