c993d68a2fb4bb21f4e63ab195d048d65ba5c685
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-05-28 23:22:14 +0700

    Update Header Components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import '@styles/components/Header.css';
 
+import headerIcon from '@assets/icons/task.png';
+import searchIcon from '@assets/icons/search.png';
+
 export default function Header() {
   return (
     <div className="header-container">
-      <h4>Header</h4>
+      <div className="header-content">
+        <img src={ headerIcon } alt="" />
+        <h3>ระบบจัดการงานซ่อม</h3>
+      </div>
+      <div className="header-content">
+        <form action="">
+          <label className="header-input-search-container">
+            <input type="text" name="qsearch" className="header-input" placeholder="Quick Search" />
+            <img src={ searchIcon } alt="" />
+          </label>
+        </form>
+      </div>
+      <div className="header-content">
+        <img src={ headerIcon } alt="" />
+        <img src={ headerIcon } alt="" />
+      </div>
     </div>
   );
 }
