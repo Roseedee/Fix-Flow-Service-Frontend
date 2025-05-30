9d292ab62db0ef7b88b7e8d4a6d3d437e2a1db1a
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-05-30 23:54:24 +0700

    update sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import '@styles/components/Sidebar.css';
+
+import appIcon from '@assets/icons/app.png';
+
 export default function Sidebar() {
   return (
     <div className="sidebar-container">
       <div className="sidebar-content">
-        <h3>Sidebar</h3>
-        <h3>Sidebar</h3>
-        <h3>Sidebar</h3>
+        <div className="sidebar-content-header">
+          <img src={ appIcon } alt="" />
+          <h2>Fix Flow Service</h2>
+        </div>
       </div>
     </div>
   );
