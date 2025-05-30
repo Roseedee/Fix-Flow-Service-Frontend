4e38248b8d87c48d6f0369c0a7d384c620d7e808
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-05-31 00:25:59 +0700

    update sidebar toggle min width

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import '@styles/components/Sidebar.css';
 
 import appIcon from '@assets/icons/app.png';
 import arrowIcon from '@assets/icons/arrow.png';
 
 export default function Sidebar() {
+  const [sidebarMin, setSidebarMin] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarMin(!sidebarMin);
+  };
+
   return (
-    <div className="sidebar-container">
+    <div className={`sidebar-container ${sidebarMin ? 'sidebar-container-min' : ''}`}>
       <div className="sidebar-content">
         <div className="sidebar-content-header">
           <img src={ appIcon } alt="" />
           <h3>FIX FLOW SERVICE</h3>
-          <div className="button-toggle-sidebar-w">
+          <div className="button-toggle-sidebar-w" onClick={ toggleSidebar }>
             <img src={ arrowIcon } alt="" />
           </div>
         </div>
