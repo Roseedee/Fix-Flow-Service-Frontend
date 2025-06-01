32e92ec877be8ead4cee1d09f9daa843cc307146
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-06-01 22:43:37 +0700

    update sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import '@styles/components/Sidebar.css';
 
 import appIcon from '@assets/icons/app.png';
 import arrowIcon from '@assets/icons/arrow.png';
+import taskIcon from '@assets/icons/checklist.png';
 
 export default function Sidebar() {
   const [sidebarMin, setSidebarMin] = useState(false);
@@ -19,6 +20,48 @@ export default function Sidebar() {
           <h3>FIX FLOW SERVICE</h3>
           <div className="button-toggle-sidebar-w" onClick={ toggleSidebar }>
             <img src={ arrowIcon } alt="" />
+          </div>
+        </div>
+        <div className="sidebar-content-menu">
+          <div className="sidebar-sub-menu">
+            <h4>ทั่วไป</h4>
+            <ul>
+              <li className="sidebar-sub-menu-item">
+                <img src={ taskIcon } alt="" />
+                <span>แดชบอร์ดงานซ่อม</span>
+              </li>
+              <li className="sidebar-sub-menu-item">
+                <img src={ taskIcon } alt="" />
+                <span>แดชบอร์ดงานซ่อม</span>
+              </li>
+              <li className="sidebar-sub-menu-item">
+                <img src={ taskIcon } alt="" />
+                <span>แดชบอร์ดงานซ่อม</span>
+              </li>
+            </ul>
+          </div>
+          <div className="sidebar-sub-menu">
+            <h4>ทั่วไป</h4>
+            <ul>
+              <li className="sidebar-sub-menu-item">
+                <img src={ taskIcon } alt="" />
+                <span>แดชบอร์ดงานซ่อม</span>
+              </li>
+              <li className="sidebar-sub-menu-item">
+                <img src={ taskIcon } alt="" />
+                <span>แดชบอร์ดงานซ่อม</span>
+              </li>
+              <li className="sidebar-sub-menu-item">
+                <img src={ taskIcon } alt="" />
+                <span>แดชบอร์ดงานซ่อม</span>
+              </li>
+            </ul>
+          </div>
+        
+        </div>
+        <div className="sidebar-content-company">
+          <div className="company-profile">
+            <img src={ appIcon } alt="" />
           </div>
         </div>
       </div>
