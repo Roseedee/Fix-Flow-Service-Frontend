9c89c2f891601c31e85abed8e80a4b51ef80c395
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-06-04 23:38:22 +0700

    update dashboard

--- src/features/Dashboard.tsx
+++ src/features/Dashboard.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Layout from '@components/Layout';
 import BarChart from '@components/BarChart';
+import '@styles/features/Dashboard.css';
 
 export default function Dashboard() {
     return (
         <Layout>
-            <div style={{ padding: '16px' }}>
+            <div className="dashboard-header">
                 <h2>Dashboard</h2>
                 <p>Welcome to the dashboard!</p>
-                <BarChart />
+            </div>
+            <div className="dashboard-content">
+                <div className="dashboard-sub-content">
+                    <BarChart />
+
+                </div>
+                <div className="dashboard-sub-content">
+                    <BarChart />
+
+                </div>
             </div>
         </Layout>
     );
