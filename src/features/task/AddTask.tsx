3af1dd7ef8ca996c1e54dc00655863f5383431b9
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-06-28 23:56:30 +0700

    update

--- src/features/task/AddTask.tsx
+++ src/features/task/AddTask.tsx
@@ -15,13 +15,14 @@ export default function AddTask() {
             </div>
             <div className="addtask-content card">
                 <div className="addtask-sub-content">
+                    <h4>ลูกค้า</h4>
                     <div className="row">
                         <div className="input-title">
-                            <span>ชื่อ-นามสกุล</span>
+                            <span>ชื่อ-นามสกุล*</span>
                             <input type="text" name="" id="" className="def-input" />
                         </div>
                         <div className="input-title">
-                            <span>เบอร์โทร</span>
+                            <span>เบอร์โทร*</span>
                             <input type="text" name="" id="" className="def-input" />
                         </div>
                     </div>
@@ -36,9 +37,10 @@ export default function AddTask() {
                         </div>
                     </div>
                     <hr />
+                    <h4>อุปกรณ์</h4>
                     <div className="row">
                         <div className="input-title">
-                            <span>ประเภทงานซ่อม</span>
+                            <span>ประเภทงานซ่อม*</span>
                             <select name="" id="" className="def-input">
                                 <option value="1">Notebook</option>
                                 <option value="1">Computer PC</option>
@@ -48,10 +50,77 @@ export default function AddTask() {
                             </select>
                         </div>
                         <div className="input-title">
-                            <span>ยี่ห้อเครื่อง</span>
+                            <span>ยี่ห้อและรุ่น*</span>
                             <input type="text" name="" id="" className="def-input" />
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="column">
+                            <div className="input-title">
+                                <span>อาการที่ลูกค้าแจ้ง*</span>
+                                <textarea name="" id="" className="def-input note"></textarea>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="input-title">
+                                <span>Product Nubmer {'(PN)'}</span>
+                                <input type="text" name="" id="" className="def-input" />
+                            </div>
+                            <div className="input-title">
+                                <span>Serial Number {'(SN)'}*</span>
+                                <input type="text" name="" id="" className="def-input" />
+                            </div>
+                        </div>
+                    </div>
+                    <hr />
+                    <h4>รายละเอียดอื่นๆ</h4>
+                    <div className="row">
+                        <div className="input-title">
+                            <span>ตำหนิ</span>
+                            <input type="text" name="" id="" className="def-input" />
+                        </div>
+                        <div className="input-title">
+                            <span>อุปกรณ์ที่ติดมาด้วย</span>
+                            <input type="text" name="" id="" className="def-input" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-title">
+                            <span>สีของอุปกรณ์</span>
+                            <input type="text" name="" id="" className="def-input" />
+                        </div>
+                        <div className="input-title">
+                            <span>รหัสผ่านอุปกรณ์</span>
+                            <input type="text" name="" id="" className="def-input" />
+                        </div>
+                    </div>
+                    <hr />
+                    <h4>รายละเอียดการซ่อม</h4>
+                    <div className="row">
+                        <div className="input-title">
+                            <span>วันที่เพิ่มงานซ่อม</span>
+                            <input type="date" name="" id="" className="def-input" />
+                        </div>
+                        <div className="input-title">
+                            <span>มัดจำเงินก่อนซ่อม</span>
+                            <input type="text" name="" id="" className="def-input" placeholder="0"/>
+                        </div>
+                        <div className="input-title">
+                            <span>ระยะเวลาในการซ่อม<span className="tag-warning">#วัน</span></span>
+                            <input type="text" name="" id="" className="def-input" placeholder="1"/>
+                        </div>
+                        <div className="input-title">
+                            <span>ราคาประเมิน</span>
+                            <input type="text" name="" id="" className="def-input" placeholder="500-1000 บาท"/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-title">
+                            <span>หมายเหตุ</span>
+                            <textarea name="" id="" className="def-input note" placeholder="หมายเหตุ"></textarea>
+                        </div>
+                    </div>
+                    <hr />
                 </div>
             </div>
         </Layout>
