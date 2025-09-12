15ac094e83a38891b9425da33a7a3380d1ea6ea7
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2025-09-13 01:33:36 +0700

    create pagination in task

--- src/features/task/AllTask.tsx
+++ src/features/task/AllTask.tsx
@@ -1,10 +1,10 @@
 import React, { use } from "react";
 import Layout from "@components/layout/Layout";
 import '@styles/features/task/AllTask.css';
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 //model
-import TaskModel, {TaskDataEx} from '@model/task/Task'
+import TaskModel, { TaskDataEx } from '@model/task/Task'
 import TaskStatus from "@model/task/TaskStatus";
 
 //components
@@ -15,41 +15,48 @@ import arrowIcon from '@assets/icons/arrow.png';
 
 export default function AllTask() {
 
+    const localtion = useLocation().pathname.split('/');
+    const [searchParams, setSearchParams] = useSearchParams();
+    let page: number = parseInt(searchParams.get('page') || '1');
+
     //input state
     const [dataLimit, setDataLimit] = React.useState<number>(() => {
         const stored = localStorage.getItem('task-dataLimit');
         return stored ? parseInt(stored) : 5;
     });
 
-    const localtion = useLocation().pathname.split('/');
+    //task state
+    const [tasks, setTasks] = React.useState<TaskModel[]>(TaskDataEx.filter(item => filterTasks(item)));
+    let tasksLenght: number = TaskDataEx.filter(item => filterTasks(item)).length;
+    let pageNumber: number = Math.ceil(tasksLenght / dataLimit);
 
-    const [Tasks, setTasks] = React.useState<TaskModel[]>();
-
-    const filterTasks = (item: TaskModel) => {
+    function filterTasks(item: TaskModel) {
         const path = localtion[localtion.length - 1];
         // console.log(path)
         if (path === 'inprogress') {
             return item.status === TaskStatus.IN_PROGRESS;
-        }else if (path === 'completed') {
+        } else if (path === 'completed') {
             return item.status === TaskStatus.COMPLETED;
-        }else if (path === 'delivered') {
+        } else if (path === 'delivered') {
             return item.status === TaskStatus.DELIVERED;
-        }else if (path === 'cancelled') {
+        } else if (path === 'cancelled') {
             return item.status === TaskStatus.CANCELLED;
-        }else if (path === 'awaitingparts') {
+        } else if (path === 'awaitingparts') {
             return item.status === TaskStatus.AWAITING_PARTS;
-        }else {
+        } else {
             return item;
         }
     }
 
     React.useEffect(() => {
-        setTasks(TaskDataEx.filter(item => filterTasks(item)).slice(0, dataLimit));
-    }, []);
+        pageNumber = Math.ceil(tasksLenght / dataLimit)
+        setTasks(TaskDataEx.filter(item => filterTasks(item)).slice((page - 1) * dataLimit, page * dataLimit));
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }, [dataLimit, page]);
+
 
     const dataLimitOnChange = (newLimit: number) => {
         setDataLimit(newLimit);
-        setTasks(TaskDataEx.filter(item => filterTasks(item)).slice(0, newLimit));
         localStorage.setItem('task-dataLimit', String(newLimit));
     }
 
@@ -63,7 +70,7 @@ export default function AllTask() {
                 <div className="alltask-sub-content">
                     <div className="alltask-sub-content-row">
                         <div className="filter-item">
-                            <select name="" id="" className="def-input" onChange={(e) => dataLimitOnChange(parseInt(e.target.value)) } value={dataLimit}>
+                            <select name="" id="" className="def-input" onChange={(e) => dataLimitOnChange(parseInt(e.target.value))} value={dataLimit}>
                                 <option value="5">แสดง 5 รายการ</option>
                                 <option value="10">แสดง 10 รายการ</option>
                                 <option value="20">แสดง 20 รายการ</option>
@@ -81,21 +88,31 @@ export default function AllTask() {
                     </div>
                     <div className="alltask-sub-content task-list">
                         {
-                            Tasks && Tasks.map((item, index) => (
+                            tasks && tasks.map((item, index) => (
                                 <TaskItem task={item} key={index} />
                             ))
                         }
                     </div>
                 </div>
                 <div className="alltask-sub-content pagination-container">
-                    <div className="pagination-item prev disabled"><img src={arrowIcon} alt="" /></div>
+                    <button className="pagination-item prev" disabled={page === 1}  onClick={() => setSearchParams({ page: (page - 1).toString() })}><img src={arrowIcon} alt="" /></button>
                     <div className="all-pagination">
-                        <div className="pagination-item active">1</div>
+                        {
+                            Array.from({ length: pageNumber }, (_, i) => (
+                                <div
+                                    key={i}
+                                    className={`pagination-item ${i + 1 === page ? "active" : ""}`}
+                                    onClick={() => setSearchParams({ page: (i + 1).toString() })}>
+                                    {i + 1}
+                                </div>
+                            ))
+                        }
+                        {/* <div className="pagination-item active">1</div>
                         <div className="pagination-item">2</div>
                         <div className="pagination-item">3</div>
-                        <div className="pagination-item">4</div>
+                        <div className="pagination-item">4</div> */}
                     </div>
-                    <div className="pagination-item next"><img src={arrowIcon} alt="" /></div>
+                    <button className="pagination-item next" disabled={page === pageNumber} onClick={() => setSearchParams({ page: (page + 1).toString() })}><img src={arrowIcon} alt="" /></button>
                 </div>
             </div>
         </Layout>
